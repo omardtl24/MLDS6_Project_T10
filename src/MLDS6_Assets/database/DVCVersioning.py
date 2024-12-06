7e115d93a85463b56f1e761ec97e9e85a2dc78af
omardtl24 <o.toledo@uniandes.edu.co>
2024-12-05 23:53:30 -0500

    Document Additional code

--- src/MLDS6_Assets/database/DVCVersioning.py
+++ src/MLDS6_Assets/database/DVCVersioning.py
@@ -2,6 +2,12 @@ import json
 import os
 
 def loadData(path):
+    '''
+    Load a file content and return it
+
+    Param:
+        path (str) : Folder where the 
+    '''
     with open(path) as f:
         return f.read()
     return None
