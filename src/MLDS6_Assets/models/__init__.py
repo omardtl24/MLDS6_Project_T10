aeee79da091ad2894eaa923abc749a504ec6a466
Rey7910 <rtoledol@unal.edu.co>
2024-12-14 23:04:05 -0500

    Include results source

--- src/MLDS6_Assets/models/__init__.py
+++ src/MLDS6_Assets/models/__init__.py
@@ -1,3 +1,4 @@
 from .split import trainTestSplitImages
 from .dataload import loadAndAugmentImagesFromDirectory, loadImagesFromDirectory
 from .models import configModelV1, configModelV2
+from .results import getModelTestResults
